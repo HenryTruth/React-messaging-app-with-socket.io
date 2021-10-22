ea7089054d2b21ad93afaa4fefa9478f581530af
Truth <henrysempire111gmail.com>
2021-10-22 09:21:25 -0700

    second commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { ContactsProvider } from "../context/ContactsProvider";
+import useLocalStorage from "../hooks/useLocalStorage";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+
+function App() {
+  const [id, setId] = useLocalStorage('id')
+
+  const dashboard = (
+    <ContactsProvider>
+      <Dashboard id={id}/>
+    </ContactsProvider>
+    )
+
+  return (
+      id ? dashboard : <Login onIdSubmit={setId}/>
+  );
+}
+
+export default App;
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,24 @@
+import {useEffect, useState} from 'react'
+
+const PREFIX = 'whatsapp-clone'
+
+export default function useLocalStorage(key, initialValue) {
+    console.log(key, 'key')
+    console.log(initialValue, 'value')
+    const prefixedKey = PREFIX + key
+    const [value, setvalue] = useState(() => {
+        const jsonValue = localStorage.getItem(prefixedKey)
+        if(jsonValue !== 'undefined' && jsonValue !== null) return JSON.parse(jsonValue)
+        if(typeof initialValue === 'function'){
+            return initialValue()
+        }else{
+            return initialValue
+        }
+    })
+
+    useEffect(() => {
+        localStorage.setItem(prefixedKey, JSON.stringify(value))
+    }, [prefixedKey, value])
+
+    return [value, setvalue]
+}
